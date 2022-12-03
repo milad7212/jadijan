9365c9bf7accb7dfdccdafa35f67b1cb221a3842
miillaadddddd <milad7212@gmail.com>
2022-12-03 19:15:12 +0330

    Add Quote component

---
+++ components/Quote.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Quote({text='من یاد گرفتن رو ازش یاد گرفتم....'}) {
+  return (
+    <div className="max-w-4xl p-4 text-gray-800 bg-white rounded-lg shadow my-2">
+        <div className="mb-2">
+          <div className="h-3 text-3xl text-left text-gray-600">“</div>
+          <p className="px-4 text-sm text-center text-gray-600">
+           {text}
+          </p>
+          <div className="h-3 text-3xl text-right text-gray-600">”</div>
+        </div>
+      </div>
+  )
+}
+
+export default Quote
--- pages/lessons/index.jsx
+++ pages/lessons/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Quote from "../../components/Quote";
 
 function LessonsPage() {
-  return <div className="bg-ciBackBeauty p-3 h-screen">
-    <div className="flex justify-center">
-            <p className="text-gray-200  font-bold ">درس هایی که آموختند از جادی</p>
-        </div>
-  </div>;
+  return (
+    <div className="bg-ciBackBeauty p-3 h-screen">
+      <div className="flex justify-center">
+        <p className="text-gray-200  font-bold ">درس هایی که آموختند از جادی</p>
+      </div>
+      <div className="flex items-center mt-4  flex-col">
+        <Quote />
+        <Quote text="من وقتی رانندگی میکردم پادکستهای جادی رو گوش میکردم. بخاطر این موضوع ازش ممنونم"/>
+      </div>
+    </div>
+  );
 }
 
 export default LessonsPage;
