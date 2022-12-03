21bd1d0c600dff33beb9397a217b9978604665b0
miillaadddddd <milad7212@gmail.com>
2022-12-03 19:00:30 +0330

    finish ui mainPage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,73 @@
 import Head from "next/head";
 import Image from "next/image";
+import { RiHeart3Fill, RiContactsBook2Fill } from "react-icons/ri";
+import { GiTeacher, GiBrickWall } from "react-icons/gi";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="bg-ciBackBeauty p-3">
-      <div className="flex justify-end flex-col items-end">
-        <Image
-          src="/images/jadi.png"
-          width={500}
-          height={500}
-          className="rounded-xl"
-        />
-        <button className="py-4 px-2 bg-ciGreen rounded-lg font-black">
-          {" "}
-          من به یادتم ...
-        </button>
+      <div className="flex items-center">
+        <RiHeart3Fill fontSize={25} className="text-ciRed animate-pulse" />
+        <p className="mr-2 text-gray-500">
+          <span className="font-bold ml-[2px]">598</span>
+          <span className=""> بار </span>
+          <span className="text-ciRed font-black mx-1">یاد تو</span>
+          <span className="">در خاطری آمد.</span>
+        </p>
+      </div>
+      <div className="flex items-center mt-4">
+        <GiTeacher fontSize={25} className="text-ciComment " />
+        <p className="mr-2 text-gray-500 flex">
+          <span className="font-bold ml-[2px]">598</span>
+          <span className="block"> درس</span>
+          <span className="text-ciComment font-black mx-1">از تو</span>
+          <span className="">به جا مانده.</span>
+        </p>
       </div>
 
-      <p className=" my-2 text-gray-50 font-bold">
-        درسی که از جادی یادم گرفتم :
-      </p>
-      <textarea
-        className="w-full rounded-lg shadow-md outline-none  p-3"
-        placeholder="...درسی که از جادی یاد گرفتم"
-        name=""
-        id=""
-        cols="5"
-        rows="5"
-      ></textarea>
+      <div className="flex  flex-col items-center">
+        <div className="relative">
+          <Image
+            src="/images/jadi.png"
+            width={500}
+            height={500}
+            className="rounded-xl"
+            alt="جادی"
+          />
+          <div className="absolute left-0 top-0 p-2 flex flex-col items-center">
+            <Link href="/wall">
+              <GiBrickWall
+                fontSize={25}
+                className="text-ciComment cursor-pointer hover:animate-ping "
+              />
+            </Link>
+            <Link href="/lessons">
+            <RiContactsBook2Fill
+              fontSize={30}
+              className="text-ciComment mt-2 cursor-pointer hover:animate-ping "
+            />
+            </Link>
+          </div>
+          <div className="absolute bottom-0">
+            <button className="py-4 px-2 bg-ciGreen shadow-lg active:scale-95 duration-200 ease-out hover:bg-ciGreen/50 rounded-lg font-black">
+              {" "}
+              من به یادتم ...
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-5xl mx-auto p-3">
+        <p className=" my-2 text-gray-400 ">درسی که از جادی یاد گرفتم :</p>
+        <textarea
+          className="w-full rounded-lg shadow-md outline-none placeholder:text-ciComment font-bold  p-3 bg-gray-200 focus-within:border-2 focus-within:ring-4"
+          placeholder="درسی که از جادی یاد گرفتم ..."
+          name=""
+          id=""
+          cols="5"
+          rows="5"
+        ></textarea>
+      </div>
     </div>
   );
 }
--- pages/wall/index.jsx
+++ pages/wall/index.jsx
@@ -3,6 +3,9 @@ import React from "react";
 function index() {
   return (
     <div className="bg-ciBackBeauty p-3 h-screen">
+        <div className="flex justify-center">
+            <p className="text-gray-200  font-bold ">دیوار زندان جادی</p>
+        </div>
       <div className="flex flex-wrap">
         <div className=" flex relative mx-2">
             <span className="h-6 w-[1px] bg-gray-200 mx-[3px]"></span>
